922a24df88e64d2d22c91ff5a0691375bf33d1e4
leonard <40259905+leonard246@users.noreply.github.com>
2019-07-07 21:20:03 +1000

    structure set

--- src/index.js
+++ src/index.js
@@ -1,16 +1,60 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './styles.css';
 
-// child class
-class Calculator extends React.Component {
+// child class of calculator
+class CalculatorButton extends React.Component {
     constructor(props) {
         super(props)
     }
 
     render() {
         return(
-            <div>
-                hello world!
+            <button type="button" className="btn btn-outline-secondary">{this.props.value}</button>    
+        )
+    }
+} 
+
+// child class
+class Calculator extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            value: 0
+        }
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleClick(e) {
+        console.log("button clicked");
+        console.log(` value: ${e.target.value}`);
+        this.setState({
+            value: e.target.value
+        });
+    }
+
+    render() {
+        return(
+            <div id="calculator">
+                <textarea id="result" placeholder="Result" disabled></textarea>
+                <br />
+                <div className="btn-group" role="group">
+                    <CalculatorButton value="1A" onClick={this.handleClick}/>
+                    <CalculatorButton value="2" />                    
+                    <CalculatorButton value="3" />
+                </div>
+                <br />
+                <div className="btn-group">
+                    <CalculatorButton value="4" />
+                    <CalculatorButton value="5" />                    
+                    <CalculatorButton value="6" />
+                </div>
+                <br />
+                <div className="btn-group">
+                    <CalculatorButton value="7" />
+                    <CalculatorButton value="8" />                    
+                    <CalculatorButton value="9" />
+                </div>
             </div>
         );
     }
@@ -23,10 +67,17 @@ class App extends React.Component {
         this.state = {
             value: ''
         }
+
     }
+
+    handleClick(e) {
+
+    }
+
     render() {
         return (
-            <div>
+            <div id="container" className="container-fluid">
+                <div id="spacer"></div>
                 <Calculator />
             </div>
         )
